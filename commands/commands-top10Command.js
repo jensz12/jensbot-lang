f4fc14a4a6001ebfff76386bb3a3daf7dfeefb92
Jens Møller <jens@jensz12.com>
2017-05-09 00:01:36 +0200

    Færdig med commands-top10Command.js

--- commands/commands-top10Command.js
+++ commands/commands-top10Command.js
@@ -1,7 +1,7 @@
 $.lang.register('top5.default', 'Top $1 $2: $3');
-$.lang.register('top5.points-disabled', 'Points are disabled.');
-$.lang.register('top5.amount.points.usage', 'usage: !topamount (amount) - Set how many people appear in the !top points list.');
-$.lang.register('top5.amount.max', 'the max amount is 15 users.');
-$.lang.register('top5.amount.points.set', '$1 users will now how in the !top command.');
-$.lang.register('top5.amount.time.usage', 'usage: !toptimeamount (amount) - Set how many people appear in the !toptime list.');
-$.lang.register('top5.amount.time.set', '$1 users will now how in the !toptime command.');
+$.lang.register('top5.points-disabled', 'Points er slået fra.');
+$.lang.register('top5.amount.points.usage', 'How-to: !topamount (antal) - Vælg antal mennesker der skal vises i !top points listen.');
+$.lang.register('top5.amount.max', 'Det maksimale er 15 brugere.');
+$.lang.register('top5.amount.points.set', '$1 brugere vil nu blive vist i !top.');
+$.lang.register('top5.amount.time.usage', 'How-to: !toptimeamount (antal) - Vælg antal mennesker der skal vises i !toptime listen.');
+$.lang.register('top5.amount.time.set', '$1 brugere vil nu blive vist i !toptime.');
