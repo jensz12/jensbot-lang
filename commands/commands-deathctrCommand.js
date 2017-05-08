5b98dab91f4aaac25fcefd147d55e751211bb97f
Jens Møller <jens@jensz12.com>
2017-05-08 18:33:32 +0200

    Vercingers Sprog filer

---
+++ commands/commands-deathctrCommand.js
@@ -0,0 +1,10 @@
+$.lang.register("deathcounter.set-error", "Provide a new valid death counter value.");
+$.lang.register("deathcounter.set-success", "Death counter i $1 blev sat til $2.");
+$.lang.register("deathcounter.add-success", "$1 er død igen i $2, hvilket bringer det totale antal op på $3.");
+$.lang.register("deathcounter.sub-success", "Trækker et dødsfald tilbage, det totale antal er nu $2 i $1.");
+$.lang.register("deathcounter.sub-zero", "The death counter for $1 is already zero, can't go any lower!");
+$.lang.register("deathcounter.counter", "$1 er død $3 gange, i $2.");
+$.lang.register("deathcounter.none", "$1 er ikke død i $2.... endnu.");
+$.lang.register("deathcounter.reset", "Reset the death counter back to 0 from $2 for $1.");
+$.lang.register("deathcounter.reset-nil", "The death counter for $1 is already 0.");
+
