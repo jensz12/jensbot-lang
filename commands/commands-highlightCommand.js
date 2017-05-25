5ae281cf432f8c674a55675ebbc51154cec14440
Lassemc475 <Lassemc123@gmail.com>
2017-05-25 16:52:16 +0200

    Stavefejl

--- commands/commands-highlightCommand.js
+++ commands/commands-highlightCommand.js
@@ -1,4 +1,4 @@
-$.lang.register('highlightcommand.highlight.offline', 'Højdepunkter kan kun idstilles når streamen er offline.');
+$.lang.register('highlightcommand.highlight.offline', 'Højdepunkter kan kun indstilles når streamen er offline.');
 $.lang.register('highlightcommand.highlight.usage', 'Anvendelse: !highlight [beskrivelse] (Bruger tiden for tidszonen: $1)');
 $.lang.register('highlightcommand.highlight.success', 'Højdepunktet blev gemt, med tiden $1.');
 $.lang.register('highlightcommand.gethighlights.no-highlights', 'Der er i øjeblikket ingen højdepunkter.');
