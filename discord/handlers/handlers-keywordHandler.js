238d270e92c46d7fbfe2cc7a17867df343c40d95
Jens Møller <jens@jensz12.com>
2017-06-18 18:28:24 +0200

    Update handlers-keywordHandler.js

--- discord/handlers/handlers-keywordHandler.js
+++ discord/handlers/handlers-keywordHandler.js
@@ -1,9 +1,9 @@
-$.lang.register('discord.keywordhandler.usage', 'Usage: !keyword [add / edit / remove]');
-$.lang.register('discord.keywordhandler.add.usage', 'Usage: !keyword add [keyword] [response]');
-$.lang.register('discord.keywordhandler.add.error', 'That keyword already exist.');
-$.lang.register('discord.keywordhandler.add.success', 'Keyword: "$1" has been added!');
-$.lang.register('discord.keywordhandler.edit.usage', 'Usage: !keyword edit [keyword] [response]');
-$.lang.register('discord.keywordhandler.404', 'That keyword does not exist.');
-$.lang.register('discord.keywordhandler.edit.success', 'Keyword: "$1" has been edited!');
-$.lang.register('discord.keywordhandler.remove.usage', 'Usage: !keyword remove [keyword]');
-$.lang.register('discord.keywordhandler.remove.success', 'Keyword: "$1" has been removed!');
+$.lang.register('discord.keywordhandler.usage', 'Anvendelse: !keyword [add / edit / remove]');
+$.lang.register('discord.keywordhandler.add.usage', 'Anvendelse: !keyword add [keyword] [svar]');
+$.lang.register('discord.keywordhandler.add.error', 'Keywodet findes allerede');
+$.lang.register('discord.keywordhandler.add.success', 'keyword "$1" blev tilføjet!');
+$.lang.register('discord.keywordhandler.edit.usage', 'Anvendelse: !keyword edit [keyword] [svar]');
+$.lang.register('discord.keywordhandler.404', 'Keywordet findes ikke.');
+$.lang.register('discord.keywordhandler.edit.success', 'Keyword: "$1" er blevet redigeret!');
+$.lang.register('discord.keywordhandler.remove.usage', 'Anvendelse: !keyword remove [keyword]');
+$.lang.register('discord.keywordhandler.remove.success', 'Keyword "$1" blev fjernet');
