4d7f430ae07058f060d7e01239ad4ed4017638e1
Jens Møller <jens@jensz12.com>
2017-06-21 13:53:23 +0200

    Update commands-nameconverterCommand.js

--- commands/commands-nameconverterCommand.js
+++ commands/commands-nameconverterCommand.js
@@ -1,4 +1,4 @@
-$.lang.register('namechange.default', 'Usage: !namechange [old name] [new name]');
-$.lang.register('namechange.updating', 'Updating name $1 to $2 in all the database tables. This may take time.');
-$.lang.register('namechange.success', 'Username $1 has been updated to $2 in $3 tables!');
-$.lang.register('namechange.notfound', 'Username $1 was not found in any tables.');
+$.lang.register('namechange.default', 'Anvendelse: !namechange [gammelt navn] [nyt navn]');
+$.lang.register('namechange.updating', 'Opdatere $1 til $2 i database tabellerne. Dette kan tage noget til');
+$.lang.register('namechange.success', '$1 er blevet opdateret til $2 i $3 tabeller!');
+$.lang.register('namechange.notfound', '$1 blev ikke fundet i nogle tabeller.');
